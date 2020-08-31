866cf58f0e045ce7e20f123eb3f083daeb05b67a
parkoon <devparkoon@gmail.com>
2020-08-31 23:36:28 +0900

    feat: complete ui

---
+++ src/components/DesktopHeader/index.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import {
+  Container,
+  Wrapper,
+  LinkedInIcon,
+  SearchInput,
+  HomeIcon,
+  NotificationIcon,
+  ProfileCircle,
+  CaretDownIcon,
+} from "./styles";
+
+const DesktopHeader: React.FC = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <div className="left">
+          <LinkedInIcon />
+          <SearchInput placeholder="..." />
+        </div>
+
+        <div className="right">
+          <nav>
+            <button className="active">
+              <HomeIcon />
+              <span>Home</span>
+            </button>
+            <button>
+              <NotificationIcon />
+              <span>Notification</span>
+            </button>
+            <button>
+              <ProfileCircle src="https://github.com/parkoon.png" />
+              <span>
+                Me <CaretDownIcon />
+              </span>
+            </button>
+          </nav>
+        </div>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default DesktopHeader;
